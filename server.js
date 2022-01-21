8ba1a3aba2009b9c98717e3d2fb1c93bba71f71d
danieljardine04 <danogaryjardine@gmail.com>
2022-01-20 19:24:20 -0700

    creating the server using Node.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+const { rootCertificates } = require('tls');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        sqlpassword: 'G@rrettGS11',
+        password: 'G@rrettGS11',
+        database: 'election'
+
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+
 
 app.get('/', (req, res) => {
     res.json({
